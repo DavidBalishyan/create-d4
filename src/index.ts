de8dc0f0e9fd329e136d353f6357acae62d44c0f
DavidBalishyan <davidbalishyan12@gmail.com>
2025-04-03 16:10:40 +0400

    changed some things

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,15 @@ async function createProject() {
     initial: "my-react-app",
   });
 
+
   const projectName = response.name;
   const targetDir = path.resolve(process.cwd(), projectName);
 
   console.log(chalk.green(`\nCreating React project with Vite...`));
 
   // Run create-vite
-  execSync(`pnpm create vite ${projectName} --template react`, {
-    stdio: "inherit",
+  execSync(`pnpm create vite ${projectName} --template react-ts`, {
+    stdio: "inherit", 
   });
 
   // Navigate to project folder
@@ -40,6 +41,28 @@ async function createProject() {
     "src/assets/tailwind.svg",
     `<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" fill="none" viewBox="0 0 256 256"><rect width="256" height="256" fill="#242938" rx="60"/><path fill="url(#paint0_linear_2_119)" fill-rule="evenodd" d="M83 110C88.9991 86.0009 104.001 74 128 74C164 74 168.5 101 186.5 105.5C198.501 108.501 209 104.001 218 92C212.001 115.999 196.999 128 173 128C137 128 132.5 101 114.5 96.5C102.499 93.4991 92 97.9991 83 110ZM38 164C43.9991 140.001 59.0009 128 83 128C119 128 123.5 155 141.5 159.5C153.501 162.501 164 158.001 173 146C167.001 169.999 151.999 182 128 182C92 182 87.5 155 69.5 150.5C57.4991 147.499 47 151.999 38 164Z" clip-rule="evenodd"/><defs><linearGradient id="paint0_linear_2_119" x1="86.5" x2="163.5" y1="74" y2="185.5" gradientUnits="userSpaceOnUse"><stop stop-color="#32B1C1"/><stop offset="1" stop-color="#14C6B7"/></linearGradient></defs></svg>`
   );
+  fs.writeFileSync(
+    "README.md",
+    `
+    # React + Vite + TS + tailwindcss v4
+    This app includes:
+    - React
+    - Vite
+    - TypeScript
+    - TailwindCSS v4
+    - Eslint
+    - React Router
+    # How do I deploy this app
+     - You can deploy this app using services like vercel, netlify, aws, etc.
+     - You can also make this app open-source using github, gitlab or Bitbucket
+    # How do I run this app
+     - For using it in development mode, run
+        "pnpm run dev"
+     - For production build run
+        "pnpm run build"
+    # Name of the project is ${projectName}
+     `
+)
 
   // Add React Router setup
   fs.writeFileSync(
@@ -62,7 +85,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://tailwindcss.com" target="_blank">
           <img src={tailwindlogo} className="logo" alt="tailwindcss logo" />
         </a>
       </div>
@@ -83,8 +106,7 @@ function App() {
 }
 
 export default App
-`
-  );
+`);
 
   console.log(chalk.green("\nSetup complete! 🎉\n"));
   console.log(chalk.yellow(`\nRun these commands to start:\n`));
